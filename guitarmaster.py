fd8c82b57e21c80bc84e2547da5443841ed351cd
anarcoiris <36089306+anarcoiris@users.noreply.github.com>
2021-07-22 17:07:30 +0200

    Update guitarmaster.py

--- guitarmaster.py
+++ guitarmaster.py
@@ -8,14 +8,30 @@ unota=""
 indexNota=[]
 i=q=j=0
 freq=0
+modif=""
 
 
 class Nota:
     def __init__(self):
         self.nota=input("Ingrese la nota tónica: ")
         self.modo=input("Introduzca el modo: ")
+        #self.modo=[]
+    #    self.modo.append(self.modoinput)
         self.p1cdq="--"
         self.p2cdq="--"
+        self.A="--"
+        self.As="--"
+        self.B="--"
+        self.C="--"
+        self.Cs="--"
+        self.D="--"
+        self.Ds="--"
+        self.E="--"
+        self.F="--"
+        self.Fs="--"
+        self.G="--"
+        self.Gs="--"
+    def freefretb(self):
         self.A="--"
         self.As="--"
         self.B="--"
@@ -45,15 +61,17 @@ class Nota:
                 indexNota.append(i)
 
                 break
-        return print(x)
+        return print(self.modo,x)
     def Obtenerpenta(self):
         j=1
         self.ordenq=int(input("Pulse 0 para no ordenar la cifra : "))
         self.penta=[]
         self.penta.append(self.freq)
+        self.escala=[]
         while j<5:
             if (1.5**j*self.freq)<440:
                 self.penta.append(1.5**j*self.freq)
+                #self.freq.append(1.5**j*self.freq)             // Por si necesito crear una nueva salida desde esta funcion
                 #print(sorted(self.penta))
                 j+=1
                 continue
@@ -78,83 +96,21 @@ class Nota:
                 #print(sorted(self.penta))
                 j+=1
                 continue
+        self.escala=list.copy(self.penta)
+    #    self.escala.sort(key = lambda x:self.escala[x]>=self.freq)
+    #    self.escala.sort(key = lambda x:self.escala[x]<self.freq,reverse=True)
+        print(list(self.escala))
+        self.etiqueta="pentatonica"
         return self
         #freq2k(self)
         #self.tonohz=self.penta[0]
-        print(list(self.penta))
         #print(list(self.cifra))
 
-    def funciontonal(self):
-        self.tonohz=int(input("Ingrese el tono: "))
-        self.intervalo=int(input("Ingrese el intervalo: "))
-        #self.tonohz=[]
-        #if self.tonohz==0:
-            #self.tonohz=input("Ingrese el tono: ")
-        tonoTeorico=self.tonohz*2**(self.intervalo/12)
-        return tonoTeorico
-
-
     def freq2ks(self):
+        #if modif!="Y":
+        #    self.escala=list.copy(self.penta)
         self.cifra=[]
-        for hertz in sorted(self.penta):
-            if hertz<225:
-                self.cifra.append("A")
-                self.A="A"
-                continue
-            elif hertz<240:
-                self.cifra.append("A#")
-                self.As="A#"
-                continue
-            elif hertz<255:
-                self.cifra.append("B")
-                self.B="B"
-                continue
-            elif hertz<270:
-                self.cifra.append("C")
-                self.C="C"
-                continue
-            elif hertz<285:
-                self.cifra.append("C#")
-                self.Cs="C#"
-                continue
-            elif hertz<305:
-                self.cifra.append("D")
-                self.D="D"
-                continue
-            elif hertz<320:
-                self.cifra.append("D#")
-                self.Ds="D#"
-                continue
-            elif hertz<340:
-                self.cifra.append("E")
-                self.E="E"
-                continue
-            elif hertz<360:
-                self.cifra.append("F")
-                self.F="F"
-                continue
-            elif hertz<380:
-                self.cifra.append("F#")
-                self.Fs="F#"
-                continue
-            elif hertz<405:
-                self.cifra.append("G")
-                self.G="G"
-                continue
-            elif hertz<425:
-                self.cifra.append("G#")
-                self.Gs="G#"
-                continue
-        print("La tónica es "+self.nota)
-        print("Las frecuencias ")
-        print(list(self.penta))
-        print("La cifra de su pentatónica ordenada ")
-        print(list(self.cifra))
-        return self
-
-    def freq2kus(self):
-        self.cifra=[]
-        for hertz in self.penta:
+        for hertz in (self.escala):
             if hertz<225:
                 self.cifra.append("A ")
                 self.A="A "
@@ -205,8 +161,72 @@ class Nota:
                 continue
         print("La tónica es "+self.nota)
         print("Las frecuencias ")
-        print(list(self.penta))
-        print("La cifra de su pentatónica natural")
+        print(list(self.escala))
+        print(f'La cifra de su escala {self.etiqueta} en clave de {self.nota} ')
+        print(list(self.cifra))
+        return self
+
+    def freq2kus(self):
+    #    if modif!="Y":
+    #        self.escala=list.copy(self.penta)
+        self.cifra=[]
+        #self.cifra.clear()
+        for hertz in self.escala:
+            if hertz<225:
+                self.cifra.append("A ")
+                self.A="A "
+                continue
+            elif hertz<240:
+                self.cifra.append("A#")
+                self.As="A#"
+                continue
+            elif hertz<255:
+                self.cifra.append("B ")
+                self.B="B "
+                continue
+            elif hertz<270:
+                self.cifra.append("C ")
+                self.C="C "
+                continue
+            elif hertz<285:
+                self.cifra.append("C#")
+                self.Cs="C#"
+                continue
+            elif hertz<305:
+                self.cifra.append("D ")
+                self.D="D "
+                continue
+            elif hertz<320:
+                self.cifra.append("D#")
+                self.Ds="D#"
+                continue
+            elif hertz<340:
+                self.cifra.append("E ")
+                self.E="E "
+                continue
+            elif hertz<360:
+                self.cifra.append("F ")
+                self.F="F "
+                continue
+            elif hertz<380:
+                self.cifra.append("F#")
+                self.Fs="F#"
+                continue
+            elif hertz<405:
+                self.cifra.append("G ")
+                self.G="G "
+                continue
+            elif hertz<425:
+                self.cifra.append("G#")
+                self.Gs="G#"
+            elif hertz<440:
+                self.cifra.append("A ")
+                self.A="A "
+                continue
+        print("La tónica es "+self.nota)
+        print("Las frecuencias ordenadas ")
+        print(list(self.escala))
+        print(f'La cifra de su escala {self.etiqueta} en clave de {self.nota} ')
         print(list(self.cifra))
         return self.cifra
 
@@ -219,17 +239,106 @@ class Nota:
             print(list(Quintas))
         return(self)
 
-    def modoJonico(self):
-        if self.posiquinta==0:
-            print("La escala jónica de Fa   F  G  A  C  Bb  D  E")
+    def funciontonal(self):
+        self.tonohz=int(input("Ingrese el tono: "))
+        self.intervalo=int(input("Ingrese el intervalo: "))
+        #self.tonohz=[]
+        #if self.tonohz==0:
+            #self.tonohz=input("Ingrese el tono: ")
+        self.tonoTeorico=self.tonohz*2**(self.intervalo/12)
+        return self.tonoTeorico
+
+    def ftonal(self,inter):
+        #self.tonohz=[]
+        #if self.tonohz==0:
+            #self.tonohz=input("Ingrese el tono: ")ç
+        self.tonoTeorico=int()
+        if (self.freq*2**(inter/12)) <430:
+            self.tonoTeorico=int(self.freq*2**(inter/12))
         else:
-            print("La escala jónica de "+self.nota)
-            self.Jonico=[]
-            self.Jonico.append(Quintas[(self.posiquinta-1)])
-            self.cifra.append(Quintas[(self.posiquinta+5)])
-            [*self.Jonico,*self.cifra]
-            print(list([*self.Jonico,*self.cifra]))
+            self.tonoTeorico=int(self.freq*2**(inter/12))
+            self.tonoTeorico=self.tonoTeorico/2
+        return self.tonoTeorico
+
+    def Modos(self):
+        self.menores=[]
+        self.mayores=[]
+        for int in self.escala:
+            if int < self.freq:
+                self.menores.append(int)
+            else:
+                self.mayores.append(int)
+        pass
+        self.mayores.sort()
+        self.menores.sort(reverse=True)
+        for int in self.menores:
+            self.mayores.append(int)
+        pass
+    #    self.nescala=list.copy(self.mayores)
+
+        self.etiqueta=self.modo
+        modif="Y"
+        if self.modo == 'jonico':
+            print("escala mayor reconocida")
+            self.intervalo=[5,11]
+            for inter in self.intervalo:
+                self.ftonal(inter)
+                self.mayores.append(self.tonoTeorico)
+                print("añadido intervalo de ")
+                print(inter)
+        if self.modo == 'dorico': #hace falta quitar notas!
+            print("modo dorico reconocido")
+            self.intervalo=[3,5,10]
+            for inter in self.intervalo:
+                if inter==5:
+                    self.ftonal(inter)
+                    self.mayores[2]=self.tonoTeorico
+                else:
+                    self.ftonal(inter)
+                    self.mayores.append(self.tonoTeorico)
+        if self.modo == "Lidio":
+            print("modo lidio reconocido")
+            self.intervalo=[6,11]
+            for inter in self.intervalo:
+                self.ftonal(inter)
+                self.mayores.append(self.tonoTeorico)
+        if self.modo == "Mixo":
+            print("modo mixolidio reconocido")
+            self.intervalo=[5,10]
+            for inter in self.intervalo:
+                self.ftonal(inter)
+                self.mayores.append(self.tonoTeorico)
+        if self.modo == 'Eolico':
+            print("modo eolico reconocido")
+            self.intervalo=[3,5,8,10]
+            for inter in self.intervalo:
+                if inter==3:
+                    self.ftonal(inter)
+                    self.mayores[2]=self.tonoTeorico
+                elif inter==8:
+                    self.ftonal(inter)
+                    self.mayores[4]=self.tonoTeorico
+                else:
+                    self.ftonal(inter)
+                    self.mayores.append(self.tonoTeorico)
+        print(list(self.mayores))
+        self.escala=self.mayores
         return self
+    #def modoJonico(self):
+    #    if self.posiquinta==0:
+        #    print("La escala jónica de Fa   F  G  A  C  Bb  D  E")
+        #    self.cifra.append("Bb")
+        #    self.Bb="Bb"
+        #    self.cifra.append("E")
+        #    self.E="E"
+    #    else:
+        #    print("La escala jónica de "+self.nota)
+        #    self.Jonico=[]
+        #    self.Jonico.append(Quintas[(self.posiquinta-1)])
+        #    self.cifra.append(Quintas[(self.posiquinta+5)])
+        #    [*self.Jonico,*self.cifra]
+        #    print(list([*self.Jonico,*self.cifra]))
+    #    return self
     def imprPaintFret(self):
         print('\n \n')
         print(f'{self.E}|--{self.F}--{self.Fs}--{self.G}--{self.Gs}--{self.A}--{self.As}--{self.B}--{self.C}--{self.Cs}--{self.D}--{self.Ds}--{self.E}--{self.F}--{self.Fs}---------------|')
@@ -247,14 +356,35 @@ nota1=Nota()
 
 nota1.ObtenerNota()
 nota1.Obtenerpenta()
-if nota1.ordenq!= 0:
-    nota1.freq2ks()
-else:
-    nota1.freq2kus()
-#nota1.freq2kus()
-nota1.funciontonal()
-nota1.modoJonico()
-nota1.circuloquintas()
+nota1.freq2kus()
 nota1.imprPaintFret()
+nota1.freefretb()
+nota1.ObtenerNota()
+nota1.Obtenerpenta()
+nota1.Modos()
+nota1.freq2kus()
+#nota1.penta.clear()
+#nota1.Obtenerpenta()
+nota1.imprPaintFret()
+nota1.Obtenerpenta()
+nota1.Modos()
+nota1.freq2ks()
+nota1.imprPaintFret()
+#if nota1.ordenq!= 0:
+#    nota1.freq2ks()
+#else:
+
+#print(list(nota1.cifra))
+#print(list(nota1.escala))
+#nota1.imprPaintFret()
+#nota1.freq2kus()
+#nota1.funciontonal()
+#nota1.modoJonico()
+#nota1.circuloquintas()
+#nota1.imprPaintFret()
+#nota2=Nota(self.nota="A")
+#nota2.imprPaintFret()
+#nota3=Nota(self.nota="As")
+#nota3.imprPaintFret()
 
 #Paso2=Escala(tnota,tmodo)
