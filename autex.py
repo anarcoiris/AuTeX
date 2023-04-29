ae612b79119843e93a5aa080537d675a9a5f9e16
anarcoiris <36089306+anarcoiris@users.noreply.github.com>
2023-04-29 13:52:23 +0200

    Update autex.py

--- autex.py
+++ autex.py
@@ -1,134 +1,93 @@
-#! python3        #PorHacer:  Sensibilidad, arreglar defectos/artefactos. Convertir en fichero de texto con extension .tex
-#AutoLateX Templater - s. anarcoiris@pm.me                    ::::===>>> implementar plantillas para completar una primera vrsión!
 import docx
-import json
-   #Prueba para arreglar el formato de las tildes, etc...
-   #sys.setdefaultencoding('utf-8')
-import sys
-#s.encode('utf-8')
+import pandas as pd
+import docx2txt
 
-orden={"Número":[],"Título":[],"Texto":[]}
-i = 0
-n = 0
-numT = 0
-Title = []
-fullText = []
-
-
-def getText(filename):
+# function to extract all paragraphs with more than 40 characters
+def get_text(filename):
     doc = docx.Document(filename)
-    #fullText = []
+    full_text = []
     for para in doc.paragraphs:
-        if len(para.text) > 40:   #Filtro de Title/Body      --- introducir parametro de sensibilidad,
-            fullText.append("\newline " + para.text)
-    return fullText            #return '\n'.join(fullText)
+        if len(para.text) > 40:
+            full_text.append("\newline " + para.text)
+    return full_text
 
-def getTitle(filename):
+# function to extract all titles (paragraphs with less than 40 characters)
+def get_title(filename):
     doc = docx.Document(filename)
-    #Title = []
-    for para in doc.paragraphs:
-        if len(para.text) < 40 and len(para.text) > 3:  #Filtro de Title
-            orden["Título"].append(" \newpage " + " \chapter{" + para.text + "}")
-    return orden #    return Title     return '\n'.join(Title)
-
-def nTitle(filename, n):  #aquí empieza la parte experimental
-    doc = docx.Document(filename)
-    Title = []
+    titles = []
     for para in doc.paragraphs:
         if len(para.text) < 40 and len(para.text) > 3:
-            Title.append(" \newpage " + " \chapter{" + para.text + "}")
-    print(Title)    #print(Title[(numT)])
-    return '\n'.join(Title[n])
+            # Add a new page and a chapter tag to each title
+            titles.append("\newpage \chapter{" + para.text + "}")
+    return titles
 
-def textChain(filename,divi):
-    claves(divi)
-    getTitle(filename)
-    i=0         #Se trata de encadenar los textos que pertenezcan al mismo capítulo.
-    doc = docx.Document(filename) #Debería añadir el vector formado por los appends anteriores a la lista cuando encuentre un paragraph de entre menos de 25 y mas de 1
-    fullText = []
-    nText = []
-    Separar=""
+# function to divide the document into chapters based on a character limit
+def get_chapters(filename, divi):
+    # get all the titles first
+    titles = get_title(filename)
+    chapters = []
+    for i, para in enumerate(doc.paragraphs):
+        if len(para.text) > 1 and len(para.text) < divi:
+            if i == 0 or len(chapters) == 0:
+                # add the first chapter with its title
+                chapters.append({'title': titles[0], 'text': []})
+            else:
+                # add the current paragraph to the latest chapter
+                chapters[-1]['text'].append("\newline " + para.text)
+        elif len(para.text) > divi:
+            raise ValueError('Paragraph too long')
+    return chapters
 
-    for para in doc.paragraphs:
-        if len(para.text) < 4:
-            #print("detectado renglón, omitiendo")
-            continue
-        elif len(para.text) > 40: #   if len(para.text) < 25 and len(para.text) > 1:
-            fullText.append(' \newline '+para.text)         #    continue
-        #    print(fullText)
-            i+=1
-            continue
-        elif i>1:                                  #print(fullText[i]) UN (posible) TITULO!
-            #print(fullText)
-            x = Separar.join(fullText) #i+=1  ¡¡ Aqui estaría juntando las strings ordenadas en una sola para asociarla a su Titulo correspondiente (EJ Capitulo 1 + párrafos 1,2,3...5)
-        #    print(fullText)
-            orden["Texto"].append(x) # Aquí quedan ordenados pEj - Parrafos del Capítulo 1, 2, 3...
-            fullText.clear()
-            monta=dict(zip(Title,nText))
-            dict(monta)
-            with open('monta.txt', 'w', encoding='utf-8') as fp:
-                json.dump(monta, fp, ensure_ascii=False)
-            dict(orden)
-            with open(filename+'.tex', 'w', encoding='utf-8') as fp:
-                json.dump(orden, fp, ensure_ascii=False)
-            #print(dict(monta))
-            #print([i for i in zip(Title,nText)])
-            #n+=1 sin uso - nTitle[n!]
-            #print(nText)
-            continue
-        else:
-            i+=1
-            continue
-
-        return print(tuple(monta))
-    #print(tuple(monta))
-    #print([i for i in zip(Title[n],nText)])
-    return monta
-#    monta = {}
-#    f = open("dict.txt","w")             TERRIBLES PRUEBAS
-#    f.write( str(dict) )
-#    f.close()
-#    orden["Título"].append()         ^^ ^^      Esta idea promete. ^^ ^^
-    #orden.keys()=Title
-    #orden[Title]
-
-
-def dumpJson(outname,filename,divi):
-    textChain(filename,divi)
-    monta=dict(zip(Title,nText))
-    dict(monta)
-    with open('monta.txt', 'w', encoding='utf-8') as fp:
-        json.dump(monta, fp, ensure_ascii=False)
-    dict(orden)
+# function to dump the chapters into a latex file
+def dump_to_latex(filename, chapters):
     with open(filename+'.tex', 'w', encoding='utf-8') as fp:
-        json.dump(orden, fp, ensure_ascii=False)
+        for chapter in chapters:
+            fp.write(chapter['title'] + '\n')
+            fp.write('\n'.join(chapter['text']) + '\n')
 
-    #print(monta)
-            # else:    #if len(para.text) < 25 and len(para.text) > 1:
-                    #    break
-    #fullText.append("'newpage " + nText)
-    #return '\n'.join(fullText)
+if __name__ == '__main__':
+    # get the file name and the character limit for dividing the chapters
+    filename = 'test.docx'
+    divi = 25
+    # get the chapters from the file
+    chapters = get_chapters(filename, divi)
+    # dump the chapters into a latex file
+    dump_to_latex(filename, chapters)
 
-def getTupla(filename):
-    getTitle(filename)
-    getText(filename)
-    zipped=zip(Title,fullText)
-    tuple(zipped)
-    print(tuple(zipped))
-    #print(list(zip(Title,fullText)))
-    print([i for i in zip(Title,fullText)])
-    return zipped
+    # Read the Word document
+    text = docx2txt.process("test.docx")
 
-def troceo(filename,n):
-    getTitle(filename)
-    getText(filename)
-    return print(list(zip(*[iter(Title)]*n)))
+    # Split the text into sections based on headings
+    sections = []
+    current_section = {"title": "", "text": ""}
+    for line in text.split("\n"):
+        if line.startswith("# "):
+            if current_section["text"]:
+                sections.append(current_section)
+                current_section = {"title": "", "text": ""}
+            current_section["title"] = line[2:]
+        else:
+            current_section["text"] += line + "\n"
+    sections.append(current_section)
 
+    # Define the template
+    template = r"""
+    \documentclass{article}
+    \begin{document}
+    {sections}
+    \end{document}
+    """
 
-def claves(divi):
-    count=0
-    while count < divi:
-        orden["Número"].append(count)
-        count+=1
-    print(orden["Número"])
-    return orden["Número"]
+    # Fill in the template
+    filled_template = template.format(
+        sections="\n\n".join(
+            "\\section{{{title}}}\n{text}".format(
+                title=section["title"], text=section["text"]
+            )
+            for section in sections
+        )
+    )
+
+    # Write the filled template to a file
+    with open("document.tex", "w") as f:
+        f.write(filled_template)
